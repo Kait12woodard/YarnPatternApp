c25453b83c4a923ae4fc5cc91ce5e4759cbe354f
Kait12woodard <kwoodard22@mail.wou.edu>
2025-09-10 11:02:21 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YarnPatternApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
